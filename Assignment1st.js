f8d312e971c3fd8e606025c12f993728852c43f6
mayur-hadake <79761126+mayur-hadake@users.noreply.github.com>
2021-08-04 12:38:56 +0530

    August Js Course

---
+++ Assignment1st.js
@@ -0,0 +1,13 @@
+// Create two variables and perform all arithmetic (addition, subtraction..etc ) operations and print the result in the console using templating syntax.
+
+let a=10
+let b=5
+console.log("conside a = "+a +" and b = " +b)
+let c =a+b
+console.log("conside a = "+a +" and b = " +b+ " addition is " +c)
+c = a-b
+console.log("conside a = "+a +" and b = " +b+ " substaction is " +c)
+c = a*b
+console.log("conside a = "+a +" and b = " +b+ " Multipliction is " +c)
+c = a/b
+console.log("conside a = "+a +" and devide by b = " +b+ " result is " +c)
